2cd3f525287d896d2b297f3566dec637b4d01543
Renato Adorno <renatowillian.2000@gmail.com>
2021-07-29 11:04:11 -0300

    finalizando ex ate o 9

---
+++ fundamentos/b4-introducao-a-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/ex6.js
@@ -0,0 +1,11 @@
+let piece = 'horse'
+
+switch (piece) {
+  case 'horse':
+    console.log('horse -> anda em L');
+  break;
+
+  case 'bishop':
+    console.log('bishop -> anda na diagonal');
+  break;
+}
